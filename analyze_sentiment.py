b9cb9bc6404d5fafc56613834cc8ab8cd92027ec
ayorba <76628516+ayorba@users.noreply.github.com>
2022-01-18 11:12:18 -0500

    Create analyze_sentiment.py

---
+++ analyze_sentiment.py
@@ -0,0 +1,2 @@
+# TODO take the user text datasets as input, and for every post/tweet add columns to the dataset corresponding with VADER metrics
+# Save the resulting DataFrame to user_sentiment_data.csv
