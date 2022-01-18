0d2cc27b6593a43bd55b185c07fd52aa53c2184d
ayorba <76628516+ayorba@users.noreply.github.com>
2022-01-18 11:16:38 -0500

    Update twitter_data_collection.py

--- twitter_data_collection.py
+++ twitter_data_collection.py
@@ -1,1 +1,13 @@
-import twython
+import tweepy
+import pandas as pd
+import numpy as np
+
+# TODO use the tweepy library to collect current twitter data from twitter
+# Twitter data is formatted as JSON objects, convert to DataFrame and save to CSV, 
+# Only save the columns: text, date, userID, followerCount, retweets, likes, comments
+# We will be using the Tweepy library because apparently nobody uses twython idk. 
+# Most online tutorials covered tweepy so we'll just use it too. not much of a difference anyway
+
+if __name__ == "__main__":
+  # write out the script here after defining necessary functions above
+  # This part of the program will actually do the stuff, but any functions you need define above.
