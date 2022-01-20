58bc7063d4dcd261860c90d49dd43021c9c71b85
ayorba <76628516+ayorba@users.noreply.github.com>
2022-01-19 23:18:05 -0500

    Create visualize_correlations.py

---
+++ visualize_correlations.py
@@ -0,0 +1,10 @@
+# TODO using pyplot, and the sentiment analyzed datasets,
+# Examine what correlations exist between user sentiment
+# And the changes that result in the crypto market
+
+import matplotlib
+
+def main():
+  
+if __name__=="__main__":
+  main()
