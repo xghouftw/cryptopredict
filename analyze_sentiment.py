b2d1f3a9d96cda7421f77878ae2fa32663af12e1
XGHou <45148024+XGHou@users.noreply.github.com>
2022-01-25 10:39:25 -0500

    Copied text only from VADER colab file from branch

--- analyze_sentiment.py
+++ analyze_sentiment.py
@@ -1,2 +1,35 @@
-# TODO take the user text datasets as input, and for every post/tweet add columns to the dataset corresponding with VADER metrics
-# Save the resulting DataFrame to user_sentiment_data.csv
+import pandas as pd
+!pip install vaderSentiment
+from google.colab import drive
+drive.mount('/content/drive')
+path = "/content/drive/MyDrive/archive.zip (Unzipped Files)/Bitcoin_tweets.csv"
+df = pd.read_csv(path)
+
+df = df.dropna()
+textcolumn = df.loc[:,"text"]
+sentlistneg = []
+sentlistneu = []
+sentlistpos = []
+sentlistcom = []
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+sid_obj = SentimentIntensityAnalyzer()
+
+def VADER(sentence):
+  sentiment_dict = sid_obj.polarity_scores(sentence)
+  sentlistneg.append(sentiment_dict['neg'])
+  sentlistneu.append(sentiment_dict['neu'])
+  sentlistpos.append(sentiment_dict['pos'])
+  sentlistcom.append(sentiment_dict['compound'])
+
+i = 0
+for comment in textcolumn:
+  VADER(comment)
+print(i)
+
+df["Neg"] = sentlistneg
+df["Neu"] = sentlistneu
+df["Pos"] = sentlistpos
+df["Com"] = sentlistcom
+
+df.to_csv("scores.csv")
